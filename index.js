6a0841b3ef46743a6835cc6d38ac9435e24ee747
Anthony Kimeu <tkymeu@gmail.com>
2022-06-10 17:48:58 +0300

    commit tree d4b224f23db9adaa73a3735d02a2fc07c7b94c34

---
+++ index.js
@@ -0,0 +1,26 @@
+const returnFirstTwoDrivers = (rfd) => {
+    return rfd.slice(0,2)
+}
+
+const returnLastTwoDrivers = (rfd) => {
+    return rfd.slice(-2)
+}
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function multiplier(num){
+    return num*num
+}
+
+const createFareMultiplier = (fare) => {
+    return function multiplier(num){
+        return fare * num 
+    }
+}
+
+const fareDoubler = createFareMultiplier(2)
+const fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers(drivers, returnLastTwoDrivers){
+    return returnLastTwoDrivers(drivers)
+}
